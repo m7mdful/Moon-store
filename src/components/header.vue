0611edd6e0a625e757279b0f039566bb0791e571
Mohammed Al Naji <ipadmohammed0@gmail.com>
2025-07-13 14:48:03 +0300

    header v1

---
+++ src/components/header.vue
@@ -0,0 +1,51 @@
+<script setup></script>
+
+<template>
+	<nav class="bg-warning py-4">
+		<div class="d-flex justify-content-around">
+			<div>
+				<img src="../assets/logo.svg" alt="" />
+			</div>
+			<div class="d-flex justify-content-around align-items-center gap-5 primary-color fsize">
+				<a href="" class="text-decoration-none primary-color" >Home</a>
+                <a href="" class="text-decoration-none primary-color">Shop</a>
+                <a href="" class="text-decoration-none primary-color">About</a>
+                <a href="" class="text-decoration-none primary-color">Contact</a>
+			</div>
+			<div class="d-flex justify-content-center align-items-center gap-3">
+                <img src="../assets/Search.svg" alt="">
+                <img src="../assets/Avatar.svg" alt="">
+                <img src="../assets/Heart.svg" alt="">
+                <img src="../assets/Vector.svg" alt="">
+            </div>
+		</div>
+	</nav>
+</template>
+
+<style scoped>
+.primary-color{
+  color:#3A3845
+}
+.primary-bg-color{
+  background-color:#3A3845;
+}
+
+.secondary-color{
+  color: #F7F6F5;
+}
+.natural-bg-color{
+  background-color: #F7F6F5;
+}
+
+.natural2-color{
+  color: #807F86;
+}
+.natural2-bg-color{
+  background-color: #807F86
+}
+
+.fsize{
+    font-size: 14px;
+}
+
+</style>

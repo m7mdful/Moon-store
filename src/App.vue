70425b588c9aed53e4b6e161c5c5420448504fea
Abdullah Albashrawi <Abdullah-albashrawi@outlook.com>
2025-07-13 21:44:54 +0300

    adding the login with the second logo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-import Header from './components/header.vue'
+
+
+
 </script>
 
 <template>
-    <Header />
+    
 </template>
 
 <style scoped>
---
+++ src/views/login.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="d-flex flex-column flex-md-row vh-100">
+    <!-- Left side -->
+    <div class="l d-flex flex-column justify-content-center align-items-center w-100 w-md-50 text-white p-4">
+      <h1><img src="../assets/logo-2.svg" alt="Logo" /></h1>
+    </div>
+
+    <!-- Right side -->
+    <div class="r w-100 w-md-50 d-flex flex-column justify-content-center align-items-center px-4 py-5">
+      
+      <p class="h1-f mb-1 text-center">Welcome Back!</p>
+      <p class="text-muted mb-5 h2-f text-center">Enter your credentials to access your account</p>
+
+      <!-- Form -->
+      <form class="w-100" style="max-width: 450px;">
+        <div class="mb-3">
+          <label for="email" class="b-f">Email Address</label>
+          <input type="email" id="email" class="form-control inputbox" />
+        </div>
+
+       
+        <div class="mb-2 text-end pe-2" style="font-size: 16px;max-width: 410px;">
+          <a href="#">Forgot password?</a>
+        </div>
+
+        <div class="mb-3">
+          <label for="pass" class="b-f">Password</label>
+          <input type="password" id="pass" class="form-control inputbox" />
+        </div>
+
+        <div class="form-check mb-4 " style="font-size: 16px;padding-left: 23px; ">
+          <input type="checkbox" id="remember" class="form-check-input " />
+          <label for="remember" class="form-check-label">Remember me</label>
+        </div>
+
+       
+        <div class="d-grid gap-3 mb-4 text-center">
+          <button type="submit" class="log-btn d-flex justify-content-center align-items-center gap-2 mb-3 text-white">Login</button>
+
+          <button type="button" class="custom-btn d-flex justify-content-center align-items-center gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48">
+              <path fill="#fbc02d" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path>
+              <path fill="#e53935" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path>
+              <path fill="#4caf50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path>
+              <path fill="#1565c0" d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
+            </svg>
+            Sign in with Google
+          </button>
+        </div>
+
+        <!-- Sign up -->
+        <p class="text-center" style="font-size: 20px;">
+          Don't have an account? <a href="#">Sign up</a>
+        </p>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// No logic yet
+</script>
+
+<style scoped>
+.l {
+  background-color: #3A3845;
+  z-index: -1;
+}
+
+.r {
+  background-color: #FFFFFF;
+}
+
+a {
+  color: ##0C2A92;
+  text-decoration: none;
+}
+p > a {
+  color:#0F3DDE
+}
+
+.h2-f {
+  font-size: 27px;
+}
+
+.h1-f {
+  font-size: 45px;
+}
+
+.b-f {
+  font-size: 24px;
+}
+
+.inputbox {
+  padding: 10px;
+  height: 37px;
+  width: 100%;
+  max-width: 404px;
+}
+
+.custom-btn {
+  height: 32px;
+  width: 100%;
+  max-width: 404px;
+  font-size: 18px;
+  padding: 10px;
+  border-radius: 10px;
+  border: 1px solid #D9D9D9;
+  background-color: white;
+  transition: all 0.3s;
+}
+.log-btn {
+  height: 32px;
+  width: 100%;
+  max-width: 404px;
+  font-size: 18px;
+  padding: 10px;
+  border-radius: 10px;
+  border: 1px solid #D9D9D9;
+  background-color: #3A3845;
+  transition: all 0.3s;
+}
+.log-btn:hover{
+
+  background-color: #3a3845ca;
+  border-color: #bbb;
+
+}
+.custom-btn:hover {
+  background-color: #f2f2f2;
+  border-color: #bbb;
+}
+img{
+  z-index: 1000;
+  width: 345px;
+  height: 136px;
+}
+</style>

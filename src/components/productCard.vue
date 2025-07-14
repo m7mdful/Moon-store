81ce98b5f654a535b39fd02c62f261c4d6d39a52
Abdullah Albashrawi <Abdullah-albashrawi@outlook.com>
2025-07-14 14:48:34 +0300

    adding the card comp & workin on shop

---
+++ src/components/productCard.vue
@@ -0,0 +1,59 @@
+<template>
+<div class="d-flex flex-column flex-md-row justify-content-around align-items-start gap-3 pt-3">
+  <div class="card product-card rounded-0 border-0">
+    <img src="../assets/img/carditem1.svg" class="card-img-top" alt="Small Ecru Compote" />
+    <div class="card-body p-0">
+      <p class="fw-bold mb-1 mt-3" style="font-size:16px;">Small Ecru Ceramic Compote</p>
+      <span class="d-block">$49.00</span>
+      <p class="card-text mt-3 mb-5 text-muted">
+        Lorem ipsum dolor sit amet conse bolli tetur adipiscing eli
+      </p>
+      <button
+        type="button"
+        class="btn w-100 text-black border-1 border-black rounded-0 fw-semibold">
+        Add to cart
+      </button>
+    </div>
+  </div>
+  <div class=" product-card rounded-0 border-0">
+    <img src="../assets/img/carditem1.svg" class="card-img-top" alt="Small Ecru Compote" />
+    <div class="card-body p-0">
+      <p class="fw-bold mb-1 mt-3" style="font-size:16px;">Small Ecru Ceramic Compote</p>
+      <span class="d-block">$49.00</span>
+      <p class="card-text mt-3 mb-5 text-muted">
+        Lorem ipsum dolor sit amet conse bolli tetur adipiscing eli
+      </p>
+      <button
+        type="button"
+        class="btn w-100 text-black border-1 border-black rounded-0 fw-semibold">
+        Add to cart
+      </button>
+    </div>
+  </div>
+</div>
+</template>
+<script>
+export default {
+}
+</script>
+<style scoped >
+.product-card {
+  width: 190px;
+  height: 500px;
+}
+.product-card > img {
+  width: 190px;
+  height: 240px;
+  object-fit: cover;
+}
+@media (min-width: 391px) {
+  .product-card {
+    width: 255px;
+    height: 530px;
+  }
+  .product-card > img {
+    width: 255px;
+    height: 321px;
+  }
+}
+</style>
--- src/views/shop.vue
+++ src/views/shop.vue
@@ -1,12 +1,91 @@
-<script setup>
-
-
-</script>
-
 <template>
-
+  <div class="d-flex justify-content-center ">
+    <!-- <p>Home / shop  </p> -->
+    <!-- checkbox div -->
+     <div class="d-flex flex-column align-items-start">
+        <p>Showing 120 items</p>
+        <hr class="w-100">
+            <!-- the first checkbox -->
+        <p class="fw-bold">Category</p>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="dinnerwave">
+            <label class="form-check-label" for="dinnerwave">DinnerWave</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="ceramic">
+            <label class="form-check-label" for="ceramic">Ceramic</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="Furniture">
+            <label class="form-check-label" for="Furniture">Furniture</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="decorart">
+            <label class="form-check-label" for="decorart">Decor Art</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="giftsets">
+            <label class="form-check-label" for="giftsets">Gift Sets</label>
+        </div>
+        <!-- second checkbox -->
+        <hr class="w-100">
+        <p class="fw-bold">Price Range</p>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="p0">
+            <label class="form-check-label" for="p0">$0 - $10</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="p10">
+            <label class="form-check-label" for="p10">$10 - $50</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="p50">
+            <label class="form-check-label" for="p50">$50 - $100</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="p100">
+            <label class="form-check-label" for="p100">$100 - $200</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="p100">
+            <label class="form-check-label" for="p100">> $200</label>
+        </div>
+        <!-- third -->
+          <hr class="w-100">
+        <p class="fw-bold">color</p>
+         <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="p100">
+            <label class="form-check-label" for="p100">sss</label>
+        </div>
+        <!-- forth  -->
+        <hr class="w-100">
+        <p class="fw-bold">Tags</p>
+         <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="dinnerwave">
+            <label class="form-check-label" for="dinnerwave">DinnerWave</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="ceramic">
+            <label class="form-check-label" for="ceramic">Ceramic</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="Furniture">
+            <label class="form-check-label" for="Furniture">Furniture</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="decorart">
+            <label class="form-check-label" for="decorart">Decor Art</label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="giftsets">
+            <label class="form-check-label" for="giftsets">Gift Sets</label>
+        </div>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-
+<script>
+export default {
+}
+</script>
+<style>
 </style>

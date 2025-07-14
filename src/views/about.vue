1c766e24849f0faee4381ec3afc1a611cc5eea59
abdullah <abdullahalbattat@outlook.com>
2025-07-14 14:31:18 +0300

    almost  about page and 4 imges

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,12 +1,134 @@
 <script setup>
 
-
 </script>
 
 <template>
+    <!-- main section -->
+    <section>
+        <div class="container-sm p-5 text-center">
+            <div class="m-auto text-wrap text-center" >
+                    <h1 class="fw-bold mb-4 ">ABOUT MOON</h1>
+                        <p class="lead text-muted px-5">Moon's handmade ceramic products have been around<br class="d-none d-md-block">since 1650, let's explore our journey</p>
+            </div> 
+        </div>
+    </section>
 
+    <!-- content -->
+
+    <!-- first col -->
+    <div class="container-xl p-0 pt-3 text-center">
+        <!-- grid system -->
+        <div class="row g-0 justify-content-center" dir="rtl">
+
+            <!-- img con -->
+            <div class="col-lg-5 ">
+                    <img src="../assets/img/about1.png" class="img-fluid" alt="">
+            </div>
+
+            <!-- text con -->
+            <div class="col-lg-5  bg-light">
+                <div class="d-flex justify-content-center align-items-center " style="height: 100%;">
+                    <div class="mx-3 p-5">
+                            <h2 class="mb-4">1910</h2>
+                            <p class="lead">Lorem ipsum dolor sit amet consectetur adipiscing eli mattis sit phasellus mollis sit aliquam sit nullam neque ultrices.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- secund col -->
+    <div class="container-xl p-0  text-center">
+        <!-- grid system -->
+        <div class="row g-0 justify-content-center" >
+
+            <!-- img cont -->
+            <div class="col-lg-5 ">
+                    <img src="../assets/img/about2.png" class="img-fluid" alt="">
+            </div>
+
+            <!-- text cont -->
+            <div class="col-lg-5  bg-light">
+                <div class="d-flex justify-content-center align-items-center " style="height: 100%;">
+                    <div class="mx-3 p-5">
+                            <h2 class="mb-4">1990</h2>
+                            <p class="lead">Maecenas sem eros, rutrum vitae risus eget, vulputate aliquam nisi. dolor sit amet consectetur adipiscing eli mattis sit phasellus mollis sit aliquam sit </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- tharid col -->
+    <div class="container-xl p-0  text-center">
+        <!-- grid system -->
+        <div class="row g-0 justify-content-center" dir="rtl">
+
+            <!-- img con -->
+            <div class="col-lg-5 ">
+                    <img src="../assets/img/about3.png" class="img-fluid" alt="">
+            </div>
+
+            <!-- text con -->
+            <div class="col-lg-5  bg-light">
+                <div class="d-flex justify-content-center align-items-center " style="height: 100%;">
+                    <div class="mx-3 p-5">
+                            <h2 class="mb-4">2010</h2>
+                            <p class="lead">Rutrum vitae risus eget, vulputate aliquam nisi ex gravida neque tempus. sit aliquam sit nullam neque ultrices.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- spase between  for large screens-->
+     <div class="d-none d-lg-block" style="margin-top: 80px;">
+     </div>
+
+     <!-- spase between  for mid to small screens-->
+     <div class="d-lg-none" style="margin-top: 40px;">
+     </div>
+
+    <!-- forth col -->
+    <div class="container-xl p-0 text-center" >
+        <!-- grid system -->
+        <div class="row g-0 justify-content-center">
+
+            <!-- img con -->
+             <div class="col-lg-5 ">
+                    <img src="../assets/img/about4.png" class="img-fluid" alt="">
+            </div>
+
+            <!-- text con -->
+             <div class="col-lg-5  bg-light">
+                <div class="d-flex justify-content-center align-items-center " style="height: 100%;">
+                    <div class="mx-3 p-5">
+                            <h2 class="mb-4">2010</h2>
+                            <p class="lead">Rutrum vitae risus eget, vulputate aliquam nisi ex gravida neque tempus. sit aliquam sit nullam neque ultrices.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+        
 </template>
 
 <style scoped>
+    /* h1 {
+        font-family:'garmond' ;
+        font-size: 36px;
+    }
+    h2 {
+        font-family:'garmond' ;
+        font-size: 28px;
+    }
+    p {
+        font-size: 16px;
+    } */
 
 </style>

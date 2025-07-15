e9016697bbfd291da14e89953695ee6b121d0a42
Mohammed Al Naji <ipadmohammed0@gmail.com>
2025-07-15 14:50:06 +0300

    Footer impove and homepage 50%

--- src/views/homepage.vue
+++ src/views/homepage.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-	<div class="d-flex flex-column">
+	<div class="d-flex flex-column gap-5">
 		<div class="position-relative">
 			<img
 				class="d-lg-flex flex-lg-column w-100"
@@ -12,18 +12,18 @@
 				class="positioning d-flex flex-column justify-content-center align-items-center gap-3 p-5"
 			>
 				<img class="imagescaling" src="../assets/Icon.svg" alt="" />
-				<p class="pcolor fw-lighter">
+				<p class="pcolor fw-light fs-5">
 					Handcrafted in Viet Nam since 1650
 				</p>
-				<h2 class="pcolor">BAT TRANG <br />DINNER SET</h2>
-				<button class="bg-white border-0 p-1 px-5 shopbtn fw-semibold">
+				<h2 class="pcolor fs-2 fw-semibold">BAT TRANG <br />DINNER SET</h2>
+				<button class="bg-white border-0 p-1 px-5 shopbtn fw-semibold fs-5">
 					SHOP NOW
 				</button>
 			</div>
 		</div>
 		<div>
 			<div
-				class="d-flex flex-wrap justify-content-evenly align-content-around p-5"
+				class="d-flex flex-wrap justify-content-center align-content-around p-5" style="gap:30px"
 			>
 				<div
 					class="d-flex flex-column justify-content-center align-items-center gap-1"
@@ -31,9 +31,9 @@
 					<img
 						src="../assets/img/Htableware.png"
 						alt=""
-						width="200"
+						
 					/>
-					<h6>TABLEWARE</h6>
+					<p class="fs-4 fw-semibold">TABLEWARE</p>
 				</div>
 				<div
 					class="d-flex flex-column justify-content-center align-items-center gap-1"
@@ -41,9 +41,9 @@
 					<img
 						src="../assets/img/Htableware.png"
 						alt=""
-						width="200"
+						
 					/>
-					<h6>TABLEWARE</h6>
+					<p class="fs-4 fw-semibold">TABLEWARE</p>
 				</div>
 				<div
 					class="d-flex flex-column justify-content-center align-items-center gap-1"
@@ -51,9 +51,9 @@
 					<img
 						src="../assets/img/Htableware.png"
 						alt=""
-						width="200"
+						
 					/>
-					<h6>TABLEWARE</h6>
+					<p class="fs-4 fw-semibold">TABLEWARE</p>
 				</div>
 				<div
 					class="d-flex flex-column justify-content-center align-items-center gap-1"
@@ -61,16 +61,46 @@
 					<img
 						src="../assets/img/Htableware.png"
 						alt=""
-						width="200"
+						
 					/>
-					<h6>TABLEWARE</h6>
+					<p class="fs-4 fw-semibold">TABLEWARE</p>
 				</div>
 			</div>
 		</div>
+        <div class="d-flex flex-lg-row flex-column justify-content-center align-content-center text-center">
+            <div class="d-flex flex-column align-items-center gap-2 resdiv bg-light" style="padding: 6rem;">
+                <h1 class="fs-3 fw-semibold">UP TO %40 OFF OUR  CHRISTMAS COLLECTION</h1>
+                <p class="fs-5">Lorem ipsum dolor sit amet consectetur adipiscing eli mattis sit phasellus mollis sit aliquam sit nullam neque ultrices.</p>
+                <button class="bg-transparent fs-6 fw-bold" style="border-bottom: 1.5px; border-style: solid; border-top: 0; border-left: 0; border-right: 0; padding-bottom: 0;">SHOP NOW</button>
+            </div>
+            <div class="overflow-hidden">
+                <img src="../assets/img/Image.png" alt="">
+            </div>
+        </div>
+        <div class="d-flex justify-content-center align-items-center">
+            <h1 class="fs-3 fw-bold">BEST SELLERS</h1>
+        </div>
+        <div class="d-flex flex-wrap justify-content-start align-content-start gap-3">
+
+        </div>
 	</div>
 </template>
 
 <style scoped>
+.resdiv{
+width: 555px;
+height: 348px;
+align-self: center;
+justify-self: center;
+@media screen and (max-width: 992px) {
+    width: auto;
+height: auto;
+align-self: center;
+justify-self: center;
+}
+}
+
+
 .positioning {
 	position: absolute;
 	top: 0;

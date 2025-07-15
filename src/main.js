768ca5bf31b1334c07a7cb365b5d85eb720d36ec
abdullah <abdullahalbattat@outlook.com>
2025-07-15 10:21:37 +0300

    add bootstrap coustmation

--- src/main.js
+++ src/main.js
@@ -5,4 +5,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import router from '../src/router/router.js';
 
+
+import "../src/assets/css/custom.min.css"; //import the compiled CSS after setting up file watcher
+import "../node_modules/bootstrap/dist/js/bootstrap"; //import bootstrap js
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js"; //import bootstrap bundle js
+
+
+
 createApp(App).use(router).mount('#app')
--- src/views/about.vue
+++ src/views/about.vue
@@ -7,82 +7,83 @@
     <section>
         <div class="container-sm p-5 text-center">
             <div class="m-auto text-wrap text-center" >
-                    <h1 class="fw-bold mb-4 ">ABOUT MOON</h1>
-                        <p class="lead text-muted px-5">Moon's handmade ceramic products have been around<br class="d-none d-md-block">since 1650, let's explore our journey</p>
+                    <h1 class="fw-bold mb-4 text-primary fs-2">ABOUT MOON</h1>
+                    <p class="lead px-5 text-secondary ">Moon's handmade ceramic products have been around<br class="d-none d-md-block">since 1650, let's explore our journey</p>
             </div> 
         </div>
     </section>
 
     <!-- content -->
+    <section>
+        <!-- first col -->
+        <div class="container-xl p-0 pt-3 text-center">
+            <!-- grid system -->
+            <div class="row g-0 justify-content-center" dir="rtl">
 
-    <!-- first col -->
-    <div class="container-xl p-0 pt-3 text-center">
-        <!-- grid system -->
-        <div class="row g-0 justify-content-center" dir="rtl">
+                <!-- img con -->
+                <div class="col-lg-5 ">
+                        <img src="../assets/img/about1.png" class="img-fluid" alt="">
+                </div>
 
-            <!-- img con -->
-            <div class="col-lg-5 ">
-                    <img src="../assets/img/about1.png" class="img-fluid" alt="">
-            </div>
-
-            <!-- text con -->
-            <div class="col-lg-5  bg-light">
-                <div class="d-flex justify-content-center align-items-center " style="height: 100%;">
-                    <div class="mx-3 p-5">
-                            <h2 class="mb-4">1910</h2>
-                            <p class="lead">Lorem ipsum dolor sit amet consectetur adipiscing eli mattis sit phasellus mollis sit aliquam sit nullam neque ultrices.</p>
+                <!-- text con -->
+                <div class="col-lg-5  bg-light">
+                    <div class="d-flex justify-content-center align-items-center " style="height: 100%;">
+                        <div class="mx-3 p-5">
+                                <h2 class="mb-4  text-primary fs-3  fw-bold ls-4">1910</h2>
+                                <p class="lead text-secondary">Lorem ipsum dolor sit amet consectetur adipiscing eli mattis sit phasellus mollis sit aliquam sit nullam neque ultrices.</p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
+            </div>
         </div>
-    </div>
 
-    <!-- secund col -->
-    <div class="container-xl p-0  text-center">
-        <!-- grid system -->
-        <div class="row g-0 justify-content-center" >
+        <!-- secund col -->
+        <div class="container-xl p-0  text-center">
+            <!-- grid system -->
+            <div class="row g-0 justify-content-center" >
 
-            <!-- img cont -->
-            <div class="col-lg-5 ">
-                    <img src="../assets/img/about2.png" class="img-fluid" alt="">
-            </div>
+                <!-- img cont -->
+                <div class="col-lg-5 ">
+                        <img src="../assets/img/about2.png" class="img-fluid" alt="">
+                </div>
 
-            <!-- text cont -->
-            <div class="col-lg-5  bg-light">
-                <div class="d-flex justify-content-center align-items-center " style="height: 100%;">
-                    <div class="mx-3 p-5">
-                            <h2 class="mb-4">1990</h2>
-                            <p class="lead">Maecenas sem eros, rutrum vitae risus eget, vulputate aliquam nisi. dolor sit amet consectetur adipiscing eli mattis sit phasellus mollis sit aliquam sit </p>
+                <!-- text cont -->
+                <div class="col-lg-5  bg-light">
+                    <div class="d-flex justify-content-center align-items-center " style="height: 100%;">
+                        <div class="mx-3 p-5">
+                                <h2 class="mb-4  text-primary fs-3 fw-bold ls-">1990</h2>
+                                <p class="lead text-secondary ">Maecenas sem eros, rutrum vitae risus eget, vulputate aliquam nisi. dolor sit amet consectetur adipiscing eli mattis sit phasellus mollis sit aliquam sit </p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
+            </div>
         </div>
-    </div>
 
-    <!-- tharid col -->
-    <div class="container-xl p-0  text-center">
-        <!-- grid system -->
-        <div class="row g-0 justify-content-center" dir="rtl">
+        <!-- tharid col -->
+        <div class="container-xl p-0  text-center">
+            <!-- grid system -->
+            <div class="row g-0 justify-content-center" dir="rtl">
 
-            <!-- img con -->
-            <div class="col-lg-5 ">
-                    <img src="../assets/img/about3.png" class="img-fluid" alt="">
-            </div>
+                <!-- img con -->
+                <div class="col-lg-5 ">
+                        <img src="../assets/img/about3.png" class="img-fluid" alt="">
+                </div>
 
-            <!-- text con -->
-            <div class="col-lg-5  bg-light">
-                <div class="d-flex justify-content-center align-items-center " style="height: 100%;">
-                    <div class="mx-3 p-5">
-                            <h2 class="mb-4">2010</h2>
-                            <p class="lead">Rutrum vitae risus eget, vulputate aliquam nisi ex gravida neque tempus. sit aliquam sit nullam neque ultrices.</p>
+                <!-- text con -->
+                <div class="col-lg-5  bg-light">
+                    <div class="d-flex justify-content-center align-items-center " style="height: 100%;">
+                        <div class="mx-3 p-5">
+                                <h2 class="mb-4 text-primary fs-3 fw-bold ls-" >2010</h2>
+                                <p class="lead text-secondary ">Rutrum vitae risus eget, vulputate aliquam nisi ex gravida neque tempus. sit aliquam sit nullam neque ultrices.</p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
+            </div>
         </div>
-    </div>
+    </section>
 
     <!-- spase between  for large screens-->
      <div class="d-none d-lg-block" style="margin-top: 80px;">
@@ -114,15 +115,15 @@
 
         </div>
     </div>
-
+    
         
 </template>
 
 <style scoped>
-    /* h1 {
-        font-family:'garmond' ;
+    .fix-titel {
         font-size: 36px;
     }
+    /*  
     h2 {
         font-family:'garmond' ;
         font-size: 28px;
